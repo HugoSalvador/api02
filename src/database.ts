f594e5331aa1355ed4f13bc3610351be643c02b4
HugoSalvador <hugossalvador1241@gmail.com>
2024-08-05 12:17:38 -0300

    subidno testes automatizados

--- src/database.ts
+++ src/database.ts
@@ -1,10 +1,11 @@
-import 'dotenv/config'
 import { knex as setupKnex, Knex } from 'knex'
 import { env } from './env'
 
 if (!process.env.DATABASE_URL) {
   throw new Error('DATABASE_URL not found')
 }
+
+console.log(process.env.DATABASE_URL)
 
 export const config: Knex.Config = {
   client: 'sqlite',
