0fad8e6e176407c0be8f77e1eb8afcacd02ca1b2
HugoSalvador <hugossalvador1241@gmail.com>
2024-08-05 14:50:32 -0300

    Lapidando a aplicação para o deploy

--- src/env/index.ts
+++ src/env/index.ts
@@ -7,6 +7,8 @@ if (process.env.NODE_ENV === 'test') {
   config()
 }
 
+console.log(process.env.PORT)
+
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
   DATABASE_CLIENT: z.enum(['sqlite', 'pg']),
